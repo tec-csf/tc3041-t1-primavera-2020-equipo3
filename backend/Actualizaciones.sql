9ba0fbd35f5f864b76b77752d749a6e03f085aab
danielroa98 <daniel.roa98@gmail.com>
2020-03-08 13:48:53 -0600

    UPDATES y DELETES

---
+++ backend/Actualizaciones.sql
@@ -0,0 +1,16 @@
+-------------------------------------------
+------------------UPDATES------------------
+-------------------------------------------
+UPDATE `partido` SET nombre = "Bailarinxs Basados en el Yunque" WHERE siglas = "BBY";
+UPDATE `votante` SET direccion = "Portón del Mixiote 34" WHERE idVotante = "KJG18BCX3WM";
+UPDATE = `votante` SET direccion = "Avenida Mataulipas 43" WHERE idVotante = "OAH87ESR2FV";
+UPDATE = `votante` SET nombre = "Margaro" WHERE idVotante = "YND29EKA6VL";
+UPDATE = `votante` SET nombre = "Grupinitia" WHERE idVotante = "EHL05SVL8GK";
+-------------------------------------------
+------------------DELETES------------------
+-------------------------------------------
+DELETE FROM `votante` WHERE idVotante = "TFQ48HCE9MO";
+DELETE FROM `votante` WHERE idVotante = "BZT38XWH1YM";
+DELETE FROM `votante` WHERE idVotante = "MMH78NRI8BK";
+DELETE FROM `votante` WHERE idVotante = "XIA96MLD1TU";
+DELETE FROM `votante` WHERE idVotante = "LOC33CMM7OM";
